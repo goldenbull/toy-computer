b748c50c433d72f65f18c50add1c5fcf8bb1045f
chenjn <chenjn@vm>
2025-07-11 20:36:33 +0800

    TODO: - jmp call ret - refactor, re-arrange modules - syntax check if needed

--- src/main.py
+++ src/main.py
@@ -8,26 +8,21 @@ from grammar.toy_asmParser import toy_asmParser
 from src.grammar.visitor_impl import VisitorImpl
 
 if __name__ == '__main__':
-    fname = "tests/1.asm"
-    txt = open(fname, "rt", encoding="utf-8").read() + '\n'  # 确保最后一行有回车
-    stm = InputStream(txt)
-    lexer = toy_asmLexer(stm)
-    stream = CommonTokenStream(lexer)
-    parser = toy_asmParser(stream)
-    visitor = VisitorImpl()
+    try:
+        fname = "tests/1.asm"
+        txt = open(fname, "rt", encoding="utf-8").read() + '\n'  # 确保最后一行有回车
+        stm = InputStream(txt)
+        lexer = toy_asmLexer(stm)
+        stream = CommonTokenStream(lexer)
+        parser = toy_asmParser(stream)
+        visitor = VisitorImpl()
 
-    # 得到所有指令
-    visitor.visit(parser.program())
-    # 启动计算机，开始逐行执行
-    computer = Computer(visitor.ops)
-    computer.run()
-    # try:
-    #     # 得到所有指令
-    #     visitor.visit(parser.program())
-    #     # 启动计算机，开始逐行执行
-    #     computer = Computer(visitor.ops)
-    #     computer.run()
-    # except Exception as ex:
-    #     print(ex)
-    #     # 语法解析错误
-    #     exit()
+        # 得到所有指令
+        visitor.visit(parser.program())
+        # 启动计算机，开始逐行执行
+        computer = Computer(visitor.ops)
+        computer.run()
+    except Exception as ex:
+        print(ex)
+        # 语法解析错误
+        exit()
