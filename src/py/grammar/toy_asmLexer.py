3b563881569e42f7953337f7b4080aa733321b9c
chenjn <chenjn@vm>
2025-07-15 09:22:18 +0800

    add C# support, hand out a stand-alone exe

--- src/py/grammar/toy_asmLexer.py
+++ src/py/grammar/toy_asmLexer.py
@@ -1,4 +1,4 @@
-# Generated from toy_asm.g4 by ANTLR 4.13.2
+# Generated from toy_asm.g4 by ANTLR 4.13.1
 from antlr4 import *
 from io import StringIO
 import sys
@@ -186,7 +186,7 @@ class toy_asmLexer(Lexer):
 
     def __init__(self, input=None, output:TextIO = sys.stdout):
         super().__init__(input, output)
-        self.checkVersion("4.13.2")
+        self.checkVersion("4.13.1")
         self._interp = LexerATNSimulator(self, self.atn, self.decisionsToDFA, PredictionContextCache())
         self._actions = None
         self._predicates = None
--- src/py/grammar/toy_asmVisitor.py
+++ src/py/grammar/toy_asmVisitor.py
@@ -1,4 +1,4 @@
-# Generated from toy_asm.g4 by ANTLR 4.13.2
+# Generated from toy_asm.g4 by ANTLR 4.13.1
 from antlr4 import *
 if "." in __name__:
     from .toy_asmParser import toy_asmParser
