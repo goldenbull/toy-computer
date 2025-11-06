43ecfa3cf22a2a3f223abaf68b83a459baaad0be
goldenbull <goldenbull@gmail.com>
2025-11-06 16:17:04 +0800

    some update

--- src/tests/nj/1.asm
+++ src/tests/nj/1.asm
@@ -1,4 +1,39 @@
-mov ax,1
-add ax,2
-add ax,3
-println ax
+
+    mov cx,0
+
+loop:
+    mov ax,cx
+    jmp function_7
+back_to_main:
+    print cx
+    print ":"
+    println ax
+
+    add cx,1
+    cmp cx,20
+    jl loop
+
+    input ax
+    jmp function_7
+back_to_main2:
+
+    println "bye!"
+    halt
+
+
+function_7:
+    div 10
+    cmp dx,7
+    je _end_with_7
+    jne _not_end_with_7
+
+_end_with_7:
+    mov ax,1
+    jmp _end_func
+
+_not_end_with_7:
+    mov ax,0
+    jmp _end_func
+
+_end_func:
+    jmp back_to_main
--- src/tests/nj/fib.asm
+++ src/tests/nj/fib.asm
@@ -1,16 +1,51 @@
-    mov cx,0
-    mov [cx],1
+    jmp main
 
-    add cx,1
-    mov [cx],1
+fib:
+    push bp
+    mov bp, sp
 
-repeat:
-    add cx,1
-    mov ax,[cx-2]
-    add ax,[cx-1]
-    mov [cx],ax
-    print [cx]
+    add sp,3
+    // [bp+0] n
+    // [bp+1] a
+    // [bp+2] b
+
+    mov [bp], ax
+    cmp ax,2
+    jle return1
+
+    mov ax,[bp]
+    sub ax,1
+    call fib
+    mov [bp+1], ax
+
+    mov ax,[bp]
+    sub ax,2
+    call fib
+    mov [bp+2], ax
+
+    print [bp]
     print " "
+    print [bp+1]
+    print " "
+    println [bp+2]
 
-    cmp cx,100
-    jl repeat
+    mov ax, [bp+1]
+    add ax, [bp+2]
+    jmp fib_ret
+
+return1:
+    mov ax,1
+
+fib_ret:
+    mov [bp],12345
+    mov [bp+1],23456
+    mov [bp+2],34567
+    mov sp,bp
+    pop bp
+    ret
+
+main:
+
+    mov ax,6
+    call fib
+    print ax
