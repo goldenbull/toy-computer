a3a5ae220b294f8415debd3e4f2752b1ca6f6cff
cjn <cjn@mac>
2026-01-07 23:36:48 +0800

    add twin prime code

---
+++ src/tests/twin-primes.asm
@@ -0,0 +1,97 @@
+_main:
+  ; 全局变量
+  ; [0] n 当前
+  ; [1] a 第一个质数
+  ; [2] b 第二个质数
+  mov cx, 0
+  mov [cx], 1        ; n保存在[0]中，从1开始
+_main_loop1:
+  ; 开始调用函数检查当前的n是不是质数
+  mov cx, 0
+  mov ax, [cx]
+  mov cx, 1
+  mov [cx], ax
+  push ax
+  call _f_is_prime
+  add sp, 1           ; 释放参数的栈空间
+  cmp ax, 1           ; 返回值为1表示是质数
+  jne _next
+
+  ; n是质数，再检查n+2是不是质数
+  mov cx, 0
+  mov ax, [cx]
+  add ax, 2
+  ;mov cx, 0
+  ;mov [cx], ax
+  mov cx, 2
+  mov [cx], ax
+  push ax
+  call _f_is_prime
+  add sp, 1           ; 释放参数的栈空间
+  cmp ax, 1           ; 返回值为1表示是质数
+  jne _next
+
+  ; 找到一对孪生质数，输出
+  mov cx, 1
+  print [cx]
+  print " "
+  mov cx, 2
+  println [cx]
+
+_next:
+  mov cx, 0
+  mov ax, [cx]
+  add ax, 2           ; n <= n+2，无需考虑2，只需要计算所有奇数
+  mov [cx], ax
+  cmp ax, 10000       ; n到10000为止
+  jg _finish
+  jmp _main_loop1
+_finish:
+  println
+  print "bye!"
+  println
+  halt
+
+_f_is_prime:
+  push bp
+  mov bp, sp
+
+  ; 参数在[bp+3]中
+  ; 质数判断的笨办法是从2一直整除到n-1
+  ; 此处使用一个简单的加速算法，只判断奇数，除数也只需要尝试奇数，且到n的平方根截止
+
+  ; 依次处理特殊情况： <2, ==2, ==3, 整除2
+  mov ax, [bp+3]
+  cmp ax, 2
+  jl _not_prime       ; <2 不是质数
+  je _is_prime        ; ==2 是质数
+  cmp ax, 3
+  je _is_prime        ; ==3 是质数
+  div 2
+  cmp dx, 0
+  je _not_prime       ; >2 且能整除2，不是质数
+
+  ; 除数从3开始，逐个尝试，直到能整除，或者商小于等于除数为止
+  mov cx, 3
+_f_is_prime_loop1:
+  mov ax, [bp+3]
+  div cx
+  cmp dx, 0
+  je _not_prime       ; 余数为0，能整除，不是质数
+  cmp ax, cx
+  jle _is_prime       ; 商小于等于除数仍然没有能整除，是质数
+  add cx, 2           ; 除数+2，继续尝试
+  jmp _f_is_prime_loop1
+
+_not_prime:
+  mov ax, 0           ; 使用ax保存返回值
+  jmp _f_is_prime_ret
+
+_is_prime:
+  mov ax, 1
+  jmp _f_is_prime_ret
+
+_f_is_prime_ret:
+  mov	sp, bp
+  pop bp
+  ret
