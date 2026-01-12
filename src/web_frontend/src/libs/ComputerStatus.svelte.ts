ac0aa6f8ee4bb6ee2b8fb702290e1d9aedd6f4b2
goldenbull <goldenbull@gmail.com>
2026-01-12 11:21:37 +0800

    remove default source code

--- src/web_frontend/src/libs/ComputerStatus.svelte.ts
+++ src/web_frontend/src/libs/ComputerStatus.svelte.ts
@@ -17,65 +17,7 @@ export enum ExecStatus {
 }
 
 export class ComputerStatus {
-    sourceCode = $state(`; Example program: find prime
-_start:
-_main:
-    mov ax, -10
-_main_loop1:
-    push ax
-    call _f_is_prime
-    mov bx, ax
-    pop ax
-    cmp bx, 1
-    jne _next
-    print ax
-    print " "
-_next:
-    add ax, 1
-    cmp ax, 1000
-    je _finish
-    jmp _main_loop1
-_finish:
-    println
-    print "bye!"
-    println
-    halt
-
-_f_is_prime:
-    push bp
-    mov bp, sp
-
-    ; check if ax is prime
-    mov ax, [bp+3]
-
-    ; check <2, ==2
-    cmp ax, 2
-    jl _not_prime
-    je _is_prime
-    div 2
-    cmp dx, 0
-    je _not_prime
-
-    ; check 3 to ax-1
-    mov cx, 3
-_f_is_prime_loop1:
-    mov ax, [bp+3]
-    cmp cx, ax
-    jge _is_prime
-    div cx
-    cmp dx, 0
-    je _not_prime
-    add cx, 1
-    jmp _f_is_prime_loop1
-_not_prime:
-    pop bp
-    mov ax, 0
-    ret
-_is_prime:
-    pop bp
-    mov ax, 1
-    ret
-`);
+    sourceCode = $state(`; 请开始你的表演 :)\n`);
 
     // Compiled operations and labels from backend
     operations = $state<Operation[]>([]);
